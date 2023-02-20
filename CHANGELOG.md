e265c9011ba7166e9256db4ea29b289a0497849d
BautisFlow <jaimebs94@gmail.com>
2023-02-20 18:16:18 +0100

    Wait for thinger to start service

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.0.1 - 2023-02-20
+### Fixed
+- Make sure Thinger server is running before starting service
+
--- task/app.js
+++ task/app.js
@@ -257,28 +257,48 @@ app.put('/settings', function (req, res) {
     return result===true ? res.sendStatus(200) : res.status(400).send({error:{message: result.message}});
 });
 
-app.listen(3000, function () {
-    console.log('HTTP Device Plugin is now running with the following configuration:');
-    console.log("HOST=" + HOST);
-    console.log("HTTT_PORT=" + HTTP_PORT);
-    console.log("HTTP_SSL_PORT=" + HTTP_SSL_PORT);
-    console.log("TOKEN=" + TOKEN);
-    console.log("USER=" + USER);
-    console.log("PLUGIN=" + PLUGIN);
-    console.log("VERSION=" + VERSION);
-    console.log("DEVELOPMENT=" + DEVELOPMENT);
+function launchServer() {
+
+    function startServer() {
+        app.listen(3000, function () {
+            console.log('HTTP Device Plugin is now running with the following configuration:');
+            console.log("HOST=" + HOST);
+            console.log("HTTT_PORT=" + HTTP_PORT);
+            console.log("HTTP_SSL_PORT=" + HTTP_SSL_PORT);
+            console.log("TOKEN=" + TOKEN);
+            console.log("USER=" + USER);
+            console.log("PLUGIN=" + PLUGIN);
+            console.log("VERSION=" + VERSION);
+            console.log("DEVELOPMENT=" + DEVELOPMENT);
+        });
+    };
 
     thinger.getPluginProperty('settings').then(function (response) {
         settings = response.data.value;
         console.log("read existing settings:",JSON.stringify(settings));
         compileCallbacks();
-    }).catch(function (error) {
-        console.error("plugin settings not available");
-        settings = {
-            'Default' : {
-                auto_provision_resources : true
-            }
-        };
-    });
 
-});
+        startServer();
+
+    }).catch(function (error) {
+
+        if ( error.response && error.response.status === 404 ) {
+          console.error("plugin settings not found");
+          settings = {
+            'Default' : {
+                auto_provision_resources : false,
+                device_response_data : '""'
+            }
+          };
+
+          startServer();
+
+        } else {
+          // Active wait until server is ready
+          console.error('server not available, checking again in 15 seconds...');
+          setTimeout( launchServer, 15000 );
+        }
+    });
+}
+
+launchServer();
